1978e036cc96eef86f40ef5d8a734605f797b945
Rahul Gururaj <rahul.gururaj96@gmail.com>
2020-09-10 18:58:21 +1000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignListComponent } from './sign-list/sign-list.component';
+import { SignComponent } from './sign/sign.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignListComponent,
+    SignComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sign-list/sign-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-list',
+  templateUrl: './sign-list.component.html',
+  styleUrls: ['./sign-list.component.scss']
+})
+export class SignListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
